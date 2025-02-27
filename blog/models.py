1df9202538e603854e8a326f657d9b9e91c3fa4c
Sajad Dehqan <109065711+sedwna@users.noreply.github.com>
2025-02-27 14:34:24 +0330

    create class Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.SlugField(max_length=100)
+    # date
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [models.Index(fields=['-published'])]
+
+    def __str__(self):
+        return self.title
