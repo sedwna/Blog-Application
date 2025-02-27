141ab38c120f7a50cddb8561b9cc854b7b3dd329
Sajad Dehqan <109065711+sedwna@users.noreply.github.com>
2025-02-27 16:07:45 +0330

    create class PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Post # *
+from .models import Post  # *
+
+
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'published')
+    ordering = ('-published',)
+    list_filter = ('status','author')
+    search_fields = ('title','description')
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published'
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ('status',)
+    # list_display_links = ('title',)
